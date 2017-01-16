fc6ad78f7fd42bb7f7b051e2830c7598867da58f
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-01-16 21:10:08 +0000

    Improve player decision making

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,6 +4,7 @@ namespace Roulette\Models;
 
 use Roulette\Roulette\Bet;
 use Roulette\Roulette\Stack;
+use Roulette\Interfaces\Doublable;
 
 class Player
 {
@@ -15,9 +16,14 @@ class Player
     private $style;
     private $id;
     private $player_won_on_previous_round;
+    private $previous_round_bet_type;
+    private $bet_type;
+    private $out_of_game;
 
     public function __construct($strategy, $amount, $style)
     {
+        $this->out_of_game = false;
+
         $this->player_won_on_previous_round = false;
 
         $this->id = (string) "id_" . bin2hex(random_bytes(15));
@@ -26,20 +32,22 @@ class Player
 
         $this->setStyle($style);
 
+        $this->setBetType();
+
         $this->stack = new Stack($amount);
 
         $this->first_go = true;
     }
 
-    private function setStrategy($strategy)
-    {
-        $class = "Roulette\\Strategies\\" . ucfirst($strategy);
-        $this->strategy = new $class();
-    }
-
     public function getId()
     {
         return $this->id;
+    }
+
+    public function setStrategy($strategy)
+    {
+        $class = "Roulette\\Strategies\\" . ucfirst($strategy);
+        $this->strategy = new $class();
     }
 
     public function getStrategy()
@@ -65,38 +73,69 @@ class Player
 
     public function makeBet()
     {
-        if ($this->player_won_on_previous_round === false && $this->first_go === false && $this->strategy->getName() == 'martingale') {
-            $this->current_bet = new \Roulette\Bets\Odds($this->stack->getDoubleAmount($this->last_bet->getAmount()));
+        if (
+            $this->player_won_on_previous_round === false &&
+            $this->first_go === false &&
+            $this->strategy instanceOf Doublable &&
+            $this->getLastBet() instanceOf Doublable
+        ) {
+            $class = get_class($this->getLastBet());
+            $this->current_bet = new $class($this->stack->getDoubleAmount($this->last_bet->getAmount()));
             $this->setLastBet($this->current_bet);
             return;
         }
         
         $this->first_go = false;
 
-        $this->current_bet = new \Roulette\Bets\StraightUp($this->stack->getAmount($this->style));
+        $class = "Roulette\\Bets\\" . ucfirst($this->bet_type);
+
+        $this->current_bet = new $class($this->stack->getAmount($this->style));
 
         $this->setLastBet($this->current_bet);
     }
 
-    public function playerWonOnPreviousRound($boolean)
+    public function previousRoundResults($boolean, $bet)
     {
         $this->player_won_on_previous_round = (boolean) $boolean;
+        $this->previous_round_bet_type = $bet;
     }
 
     public function isActive()
     {   
-        if ($this->stack->getRemainingStack() >= $this->stack->getInitialStack() * 10) {
-            var_dump("player wins big!");
-            var_dump($this->stack->getRemainingStack());
+        if ($this->out_of_game) {
+            return false;
+        }
+
+        if ($this->stack->getRemainingStack() >= $this->stack->getInitialStack() * 5) {
+            echo "big win! player " . $this->getId() . " wins $" . $this->stack->getRemainingStack() . "\n";
+            $this->out_of_game = true;
             return false;
         }
 
         if ($this->stack->getRemainingStack() <= 0) {
-            var_dump("house wins, sorry buddy.");
-            var_dump($this->stack->getRemainingStack());
+            echo "house wins, sorry buddy. player " . $this->getId() . " loses everything" . "\n";
+            $this->out_of_game = true;
             return false;
         }
 
         return true;
+    }
+
+    public function setBetType()
+    {
+        if ($this->strategy instanceOf Doublable) {
+            $array = [
+                // 'red',
+                // 'black',
+                'odds',
+                'evens',
+            ];
+            $k = array_rand($array);
+            $bet = $array[$k];
+        } else {
+            $bet = 'straightUp';
+        }
+
+        $this->bet_type = $bet;
     }
 }
