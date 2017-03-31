cda0fc5019e0ee166de9a02c6f5017f60f93dcc9
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-03-31 13:33:05 +0100

    Fix broken player tests

--- tests/unit/Models/PlayerTest.php
+++ tests/unit/Models/PlayerTest.php
@@ -5,35 +5,35 @@ use PHPUnit\Framework\TestCase;
 class PlayerTest extends TestCase
 {
     protected $player;
+    protected $none;
+    protected $cautious;
+
+    public function __construct()
+    {
+        $this->none = new \Roulette\Strategies\None;
+        $this->cautious = new \Roulette\Styles\Cautious;
+    }
 
     public function setUp()
     {
-        $this->player = new \Roulette\Models\Player('none', 100, 'cautious');
-    }
-
-    /** @test */
-    public function that_player_is_not_active_until_they_stake_themselves()
-    {
-        $this->assertTrue($this->player->isActive());
+        $this->player = new \Roulette\Players\Player($this->none, 100, $this->cautious);
     }
 
     /** @test */
     public function that_player_is_active_whilst_they_still_have_money()
     {
-        $this->player->stack->getAmount();
-        $this->assertTrue($this->player->isActive());
+        $player = new \Roulette\Players\Player($this->none, 20, $this->cautious);
+
+        $this->assertTrue($player->isActive());
     }
 
     /** @test */
     public function that_player_is_not_active_when_they_have_no_more_money_to_play_with()
     {
-        $this->player->stack->getAmount();
-        $this->assertFalse($this->player->isActive());
-    }
+        $player = new \Roulette\Players\Player($this->none, 100, $this->cautious);
 
-    /** @test */
-    public function strategy_is_set_for_player()
-    {
-        $this->assertInstanceOf(Roulette\Strategies\None::class, $this->player->getStrategy());
+        $player->stack->addToRemainingStack(1000);
+
+        $this->assertFalse($player->isActive());
     }
 }
