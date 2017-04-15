9f45384e3c456f3e92d868509bffe8a25a188b2c
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-04-15 21:37:05 +0100

    Fix tests

--- tests/unit/Models/PlayerTest.php
+++ tests/unit/Models/PlayerTest.php
@@ -4,6 +4,8 @@ use PHPUnit\Framework\TestCase;
 
 use Roulette\Players\Player;
 use Roulette\Players\Stack;
+use Roulette\Library\Adapters\Logger;
+use Roulette\Library\Adapters\StreamHandler;
 
 class PlayerTest extends TestCase
 {
@@ -19,7 +21,7 @@ class PlayerTest extends TestCase
     /** @test */
     public function that_player_is_active_whilst_they_still_have_money()
     {
-        $player = new Player($this->none, new Stack(20, $this->cautious), $this->cautious);
+        $player = new Player($this->none, new Stack(20, $this->cautious), $this->cautious, $this->logger());
 
         $this->assertTrue($player->isActive());
     }
@@ -27,10 +29,21 @@ class PlayerTest extends TestCase
     /** @test */
     public function that_player_is_not_active_when_they_have_no_more_money_to_play_with()
     {
-        $player = new Player($this->none, new Stack(100, $this->cautious), $this->cautious);
+        $player = new Player($this->none, new Stack(100, $this->cautious), $this->cautious, $this->logger());
 
         $player->stack->addToRemainingStack(1000);
 
         $this->assertFalse($player->isActive());
+    }
+
+    private function logger()
+    {
+        $logger = new Logger('Simulator');
+        $logger->pushHandler(
+            new StreamHandler('../logs/main.log',
+            Logger::INFO)
+        );
+
+        return $logger;
     }
 }
